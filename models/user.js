f83a2ae62edc5bd604e75ce79f15a445a295f480
furkancelik <celikfurkan45@hotmail.com>
2025-11-11 18:21:31 +0300

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+// Parola hash'leme için pre-save hook'u
+// (index.js'de manuel hash'leme yapmadıysak bu gereklidir)
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+export const User = model("user", userSchema);
